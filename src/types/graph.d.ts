f012d437c12065a0777e6b485ecc742f093895a4
heeinso <heeinso@snu.ac.kr>
2018-07-31 15:59:41 +0900

    Define resolver types

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,6 +1,27 @@
-export const typeDefs = ["type Query {\n  sayHello: String!\n}\n"];
+export const typeDefs = ["type Mandalart {\n  id: Int!\n  name: String!\n  content: [String]!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype User {\n  id: Int!\n  email: String\n  password: String\n  firstName: String\n  lastName: String\n  age: Int\n  profileImage: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n}\n\ntype Query {\n  user: User\n}\n"];
 /* tslint:disable */
 
 export interface Query {
-  sayHello: string;
+  user: User | null;
+}
+
+export interface User {
+  id: number;
+  email: string | null;
+  password: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  age: number | null;
+  profileImage: string | null;
+  createdAt: string;
+  updatedAt: string | null;
+  fullName: string | null;
+}
+
+export interface Mandalart {
+  id: number;
+  name: string;
+  content: Array<string>;
+  createdAt: string;
+  updatedAt: string | null;
 }
