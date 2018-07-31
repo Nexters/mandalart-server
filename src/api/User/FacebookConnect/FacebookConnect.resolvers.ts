2f97d7c11901c50570cdba8625629c705ce2a52c
heeinso <heeinso@snu.ac.kr>
2018-07-31 18:47:03 +0900

    Create FacebookConnect resolver

---
+++ src/api/User/FacebookConnect/FacebookConnect.resolvers.ts
@@ -0,0 +1,53 @@
+import { Resolvers } from '../../../types/resolvers';
+import {
+  FacebookConnectMutationArgs,
+  FacebookConnectResponse,
+} from '../../../types/graph';
+import User from '../../../entities/User';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    FacebookConnect: async (
+      _,
+      args: FacebookConnectMutationArgs
+    ): Promise<FacebookConnectResponse> => {
+      const { fbId } = args;
+      try {
+        const existingUser = await User.findOne({ fbId });
+        if (existingUser) {
+          return {
+            ok: true,
+            error: null,
+            token: 'not yet',
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+
+      try {
+        await User.create({
+          ...args,
+          profileImage: `http://graph.facebook.com/${fbId}/picture?type=square`,
+        }).save();
+        return {
+          ok: true,
+          error: null,
+          token: 'not yet',
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+    },
+  },
+};
+
+export default resolvers;
