a47937d1e0874fa8f7b1a5753a7a84ccb1ede327
heeinso <heeinso@snu.ac.kr>
2018-08-10 18:07:19 +0900

    Implement Add Todo mutation

--- src/api/Todo/AddTodo/AddTodo.resolvers.ts
+++ src/api/Todo/AddTodo/AddTodo.resolvers.ts
@@ -3,6 +3,7 @@ import User from '../../../entities/User';
 import { AddTodoMutationArgs, AddTodoResponse } from '../../../types/graph';
 import { Resolvers } from '../../../types/resolvers';
 import privateResolver from '../../../utils/privateResolver';
+import Mandalart from '../../../entities/Mandalart';
 
 const resolvers: Resolvers = {
   Mutation: {
@@ -14,15 +15,37 @@ const resolvers: Resolvers = {
       ): Promise<AddTodoResponse> => {
         const user: User = req.user;
         try {
-          await Todo.create({ ...args, user }).save();
-          return {
-            ok: true,
-            error: null,
-          };
+          const mandalart = await Mandalart.findOne({ id: args.mandalartId });
+          if (mandalart) {
+            if (mandalart.userId === user.id) {
+              const todo = await Todo.create({
+                ...args,
+                user,
+              }).save();
+              return {
+                ok: true,
+                error: null,
+                todo,
+              };
+            } else {
+              return {
+                ok: false,
+                error: 'Unauthorized',
+                todo: null,
+              };
+            }
+          } else {
+            return {
+              ok: false,
+              error: 'Todo not found',
+              todo: null,
+            };
+          }
         } catch (error) {
           return {
             ok: false,
             error: error.message,
+            todo: null,
           };
         }
       }
